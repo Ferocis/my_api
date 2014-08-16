5fad733859fb86a0fd0d246f53764c00a3083a18
Ferocis <kamszyn@gmail.com>
2014-08-16 19:14:58 +0200

    za kazdym razem, gdy robie cos rownie glupiego, umiera jednorozec

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-	resources :twitts
+	resources :tweets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'twitts#index'
+   root 'tweets#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
