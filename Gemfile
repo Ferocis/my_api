479be673899eb028e9d54a5981fb7d25c2c50521
Ferocis <kamszyn@gmail.com>
2014-08-18 11:46:07 +0200

    dodalem search i podswietlanie linkow

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 
+gem 'twitter-bootstrap-rails'
+gem 'twitter-text'
 gem 'twitter'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ app/models/tweet.rb
@@ -0,0 +1,18 @@
+class Tweet < ActiveRecord::Base
+	def self.search(search)
+		client = Twitter::REST::Client.new do |config|
+		config.consumer_key = 'rkv9vy0QcRY35Yw5Zq9aD2WMW'
+		config.consumer_secret = 'jIIvit7otJeRsIhsBjA8GEQMXdyCBk55gy2ljQw9GOjJDhbXov'
+		config.access_token = '2737233824-r9UnPv2NyzTQGAIxv7FKGgDq4QWHFxQ97DnxqwG'
+		config.access_token_secret = 'o7wZgITAcpGvt4TTVvVOrMtQKPTVwDGIu49xP7oINkbKB'
+	end
+	options = {:count => 50, :include_rts => true}
+
+	begin
+		client.user_timeline(search, options)	
+	rescue => ex
+		client.user_timeline("", options)
+	end
+	
+	end	
+end
