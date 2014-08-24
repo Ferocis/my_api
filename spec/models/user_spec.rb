795b74f212b7291957d84010aadc8f0a02fdf629
Ferocis <kamszyn@gmail.com>
2014-08-24 13:09:21 +0200

    add travis.yml

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
 describe User do
-#  let(:user) { build(:user) }
-  before do
-    OmniAuth.config.mock_auth[:twitter] = {
-	'uid' => '12345'
-    
-    }
 
-  end
-  it "should not have nil config" do
-    
-  end
 end
