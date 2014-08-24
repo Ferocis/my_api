77355baae3ba745e7028b9e47f8ee799e5cde26b
Ferocis <kamszyn@gmail.com>
2014-08-25 01:52:46 +0200

    zmiana danych dostępowych

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,10 @@
 class Tweet < ActiveRecord::Base
 	def self.search(search)
 		client = Twitter::REST::Client.new do |config|
-		config.consumer_key = 'rkv9vy0QcRY35Yw5Zq9aD2WMW'
-		config.consumer_secret = 'jIIvit7otJeRsIhsBjA8GEQMXdyCBk55gy2ljQw9GOjJDhbXov'
-		config.access_token = '2737233824-r9UnPv2NyzTQGAIxv7FKGgDq4QWHFxQ97DnxqwG'
-		config.access_token_secret = 'o7wZgITAcpGvt4TTVvVOrMtQKPTVwDGIu49xP7oINkbKB'
+		config.consumer_key = 'PHrxW95nLpqjiB9dXpwf8TyN7'
+		config.consumer_secret = 'ffuXwJLNcrLHduGYXb3YGqDIamx11DdgpYOuhQ6jEO0BeNtrks'
+		config.access_token = '2737233824-IJLpN5aJDZBzuSkEvipUv2BqNZnMxnPtjcpgTWM'
+		config.access_token_secret = 'tcCtiP50Tfb0UG6B6NjMFwBXfxrHAuZ5McPMuA4uHwx6g'
 	end
 	options = {:count => 50, :include_rts => true}
 
